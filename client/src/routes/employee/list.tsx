72463ef8d15246b12443c72d840df984cba810ee
Deepakpmk007 <deepakpmk@hotmail.com>
2025-12-27 15:19:09 +0530

    add some navigation add new employee

--- client/src/routes/employee/list.tsx
+++ client/src/routes/employee/list.tsx
@@ -1,10 +1,11 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import {
   flexRender,
   getCoreRowModel,
   getSortedRowModel,
   useReactTable,
 } from '@tanstack/react-table'
+import { Plus } from 'lucide-react'
 
 export const Route = createFileRoute('/employee/list')({
   component: RouteComponent,
@@ -51,16 +52,25 @@ function RouteComponent() {
 
   console.log(table.getHeaderGroups())
   return (
-    <div className="p-2 flex flex-col gap-4 w-full bg-blue-200">
-      <section>Header</section>
+    <div className="p-2 flex flex-col gap-4 w-full font-mono">
+      <section className="p-2 flex justify-between items-center">
+        <h1>Employees List</h1>
+        <Link
+          to={'/employee/add'}
+          className="flex gap-3 items-center bg-green-300 hover:bg-green-400 transition-all cursor-pointer px-4 py-2 rounded"
+        >
+          <Plus size={18} />{' '}
+          <span className="font-medium ">App new employee</span>
+        </Link>
+      </section>
       <section className="flex-1 p-4">
-        <table className="table-auto border-collapse border border-slate-400 w-full">
+        <table className="table-auto border-collapse w-full">
           <thead>
             {table
               .getHeaderGroups()
               .map((headergroup) =>
                 headergroup.headers.map((header) => (
-                  <th className="text-start">
+                  <th className="text-start p-4 border-b border-slate-300 uppercase">
                     {flexRender(
                       header.column.columnDef.header,
                       header.getContext(),
@@ -73,7 +83,7 @@ function RouteComponent() {
           {table.getRowModel().rows.map((row) => (
             <tr key={row.id}>
               {row.getVisibleCells().map((cell) => (
-                <td key={cell.id} className="border border-slate-300 p-2">
+                <td key={cell.id} className="border-b border-slate-300 p-4">
                   {flexRender(cell.column.columnDef.cell, cell.getContext())}
                 </td>
               ))}
