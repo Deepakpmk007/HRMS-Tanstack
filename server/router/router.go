c69df492d1412b83543c0d09d7a1dfdf98d687bd
Deepakpmk007 <deepakpmk@hotmail.com>
2025-12-29 14:11:33 +0530

    create user route

--- server/router/router.go
+++ server/router/router.go
@@ -1,12 +1,16 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/Deepak007/HRMS-Tanstack/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func SerRouter(r *gin.Engine) {
-	// api := r.Group("/api/v1/");
+	api := r.Group("/api/v1/");
 
-	// // user := api.Group("/user")
-	// // {
-
-	// // }
+	user := api.Group("/user")
+	{
+		user.GET("/",controller.GetAllUsers)
+		user.GET("/:id",controller.GetUser)
+	}
 }
